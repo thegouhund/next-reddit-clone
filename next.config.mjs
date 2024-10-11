aa8c983cf652510f99a5879581813402171a67d2
thegouhund <alexandresapu1@gmail.com>
2024-10-11 11:36:05 +0800

    chore: remove sequelize and unused db files

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["sequelize"],
-  },
   images: {
     domains: ["lh3.googleusercontent.com", "cdn.discordapp.com"],
   },
