d6a19aceaa988c91d7268a01db9d0a0d0495b36e
thegouhund <alexandresapu1@gmail.com>
2024-10-12 01:50:05 +0800

    fix(config): replaced deprecated nextjs image.domains to remotePatterns
    https://nextjs.org/docs/pages/api-reference/components/image#domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "cdn.discordapp.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        pathname: '**',
+      },
+    ],
   },
 };
 
