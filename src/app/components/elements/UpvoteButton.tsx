b4c5a59ead4e5f80e1a7dd8179b92f01fd18ba73
thegouhund <alexandresapu1@gmail.com>
2024-10-20 19:58:45 +0800

    refactor(Upvote): consolidate upvote and downvote logic

--- src/app/components/elements/UpvoteButton.tsx
+++ src/app/components/elements/UpvoteButton.tsx
@@ -1,7 +1,7 @@
 import { PostWithUserAndSubbedit } from "@/app/types/post";
 import { Vote as VoteType } from "@prisma/client";
 import { useSession } from "next-auth/react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { ArrowDownShort, ArrowUpShort } from "react-bootstrap-icons";
 
 interface UpvoteButtonProps {
@@ -9,14 +9,14 @@ interface UpvoteButtonProps {
 }
 
 const UpvoteButton: React.FC<UpvoteButtonProps> = ({ post }) => {
-  const [upvote, setUpvote] = useState<number>(
-    post.Vote.reduce(
-      (total, vote) => (vote.isUpvote ? total + 1 : total - 1),
+  const [voteState, setVoteState] = useState({
+    upvote: post.Vote.reduce(
+      (total, vote) => total + (vote.isUpvote ? 1 : -1),
       0,
     ),
-  );
-  const [isDownvoted, setIsDownvoted] = useState<boolean>(false);
-  const [isUpvoted, setIsUpvoted] = useState<boolean>(false);
+    isUpvoted: false,
+    isDownvoted: false,
+  });
   const { data: session } = useSession();
 
   useEffect(() => {
@@ -24,101 +24,59 @@ const UpvoteButton: React.FC<UpvoteButtonProps> = ({ post }) => {
       (v) => v.userId === parseInt(session?.user.id || "0"),
     );
     if (userVote) {
-      setIsUpvoted(userVote.isUpvote);
-      setIsDownvoted(!userVote.isUpvote);
-    } else {
-      setIsUpvoted(false);
-      setIsDownvoted(false);
+      setVoteState((prev) => ({
+        ...prev,
+        isUpvoted: userVote.isUpvote,
+        isDownvoted: !userVote.isUpvote,
+      }));
     }
   }, [post.Vote, session?.user]);
 
-  const handleUpvote = async () => {
-    if (isUpvoted) {
-      const data = await (
-        await fetch(`/api/post/vote/${post.id}`, {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ postId: post.id }),
-        })
-      ).json();
+  const handleVote = useCallback(
+    async (isUpvote: boolean) => {
+      const { isUpvoted, isDownvoted } = voteState;
+      const currentVote = isUpvote ? isUpvoted : isDownvoted;
 
-      console.log(data);
-      setUpvote(upvote - 1);
-      setIsUpvoted(false);
-      setIsDownvoted(false);
-      return;
-    }
+      const method = currentVote ? "DELETE" : "POST";
+      let voteChange;
+      if (currentVote) {
+        voteChange = isUpvote ? -1 : 1;
+      } else {
+        voteChange = isUpvote ? (isDownvoted ? 2 : 1) : isUpvoted ? -2 : -1;
+      }
 
-    if (isDownvoted) setUpvote(upvote + 1);
+      const response = await fetch(`/api/post/vote/${post.id}`, {
+        method,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          postId: post.id,
+          isUpvote: !currentVote && isUpvote,
+        }),
+      });
 
-    const data = await (
-      await fetch(`/api/post/vote/${post.id}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ postId: post.id, isUpvote: true }),
-      })
-    ).json();
-
-    console.log(data);
-    setUpvote((prev) => prev + 1);
-    setIsUpvoted(true);
-    setIsDownvoted(false);
-  };
-
-  const handleDownvote = async () => {
-    if (isDownvoted) {
-      const data = await (
-        await fetch(`/api/post/vote/${post.id}`, {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ postId: post.id }),
-        })
-      ).json();
-
-      console.log(data);
-      setUpvote(upvote + 1);
-      setIsDownvoted(false);
-      setIsUpvoted(false);
-      return;
-    }
-
-    if (isUpvoted) setUpvote(upvote - 1);
-
-    const data = await (
-      await fetch(`/api/post/vote/${post.id}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ postId: post.id, isUpvote: false }),
-      })
-    ).json();
-
-    console.log(data);
-    setUpvote((prev) => prev - 1);
-    setIsDownvoted(true);
-    setIsUpvoted(false);
-    return;
-  };
+      if (response.ok) {
+        setVoteState((prev) => ({
+          upvote: prev.upvote + voteChange,
+          isUpvoted: !currentVote && isUpvote,
+          isDownvoted: !currentVote && !isUpvote,
+        }));
+      }
+    },
+    [post.id, voteState],
+  );
 
   return (
     <div className="flex items-center gap-2 rounded-lg bg-gray-200 p-1 hover:bg-gray-300">
       <button
-        onClick={handleUpvote}
-        className={`${isUpvoted && "text-blue-500"} rounded-lg border border-transparent hover:border-gray-400`}
+        onClick={() => handleVote(true)}
+        className={`${voteState.isUpvoted ? "text-blue-500" : ""} rounded-lg border border-transparent hover:border-gray-400`}
       >
         <ArrowUpShort size={20} />
       </button>
-      <p>{upvote}</p>
+      <p>{voteState.upvote}</p>
       <button
-        onClick={handleDownvote}
-        className={`${isDownvoted && "text-red-500"} rounded-lg border border-transparent hover:border-gray-400`}
+        onClick={() => handleVote(false)}
+        className={`${voteState.isDownvoted ? "text-red-500" : ""} rounded-lg border border-transparent hover:border-gray-400`}
       >
         <ArrowDownShort size={20} />
       </button>
