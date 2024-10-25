29ff44b1bba48a8f809b33d868b7e8149889756e
thegouhund <alexandresapu1@gmail.com>
2024-10-25 17:00:45 +0800

    feat: add uploadthing endpoint to upload images

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,24 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '**',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-        pathname: '**',
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        pathname: '**',
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "**",
       },
     ],
   },
